b18ae225995a7f6830585857235aae5132aacdc4
stl <73641317+stl-23@users.noreply.github.com>
2021-10-13 10:00:37 +0800

    Update mutirun.py

--- mutirun.py
+++ mutirun.py
@@ -4,37 +4,55 @@ import sys
 import subprocess
 from multiprocessing.dummy import Pool as ThreadPool
 
+#def run_shell_cmd(cmd):
+#    run = subprocess.run(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+#    if run.returncode == 0:
+#        print("STDOUT:", run.stdout)
+#    else:
+#        print("STDOUT:", run.stdout)
+#        print("STDERR:", run.stderr)
 def run_shell_cmd(cmd):
-    run = subprocess.run(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    if run.returncode == 0:
-        print("STDOUT:", run.stdout)
-    else:
-        print("STDOUT:", run.stdout)
-        print("STDERR:", run.stderr)
+    ## code from https://github.com/XWangLabTHU/cfDNApipe/blob/master/cfDNApipe/cfDNA_utils.py
+    proc = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True,
+    )
+    while True:
+        nextline = proc.stdout.readline()
+        if (nextline == "") and (proc.poll() is not None):
+            break
+        sys.stdout.write(nextline)
+        sys.stdout.flush()
 
-def multi_run(func,cmds,jobs,maxc):
+    output, error = proc.communicate()
+    exitCode = proc.returncode
+
+    if exitCode != 0:
+        print(output)
+        print(error)
+
+def multi_run(func,cmds,jobs):
     ## split command into sub-commands,
     ## each sub-command has {jobs} tasks
     ## run {jobs} tasks parallelly to save time
     if isinstance(cmds,list):
         for i in range(0, len(cmds), jobs):
             subcmds = cmds[i:i + jobs]
-            pool = ThreadPool(maxc)
+            pool = ThreadPool(jobs)
             pool.map(func, subcmds)
             pool.close()
             pool.join()
     elif isinstance(cmds,str):
         new_cmds = [cmds]
-        pool = ThreadPool(maxc)
+        pool = ThreadPool(jobs)
         pool.map(func, new_cmds)
         pool.close()
         pool.join()
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("python download.py command_file job_number thread_counts")
+    if len(sys.argv) != 3:
+        print("python download.py command_file job_number")
         exit(0)
     cmds = [i.strip() for i in open(sys.argv[1]).readlines()]
     jobs = int(sys.argv[2])
-    thread_counts = int(sys.argv[3])
-    multi_run(run_shell_cmd,cmds,jobs,thread_counts)
+    #thread_counts = int(sys.argv[3])
+    multi_run(run_shell_cmd,cmds,jobs)
